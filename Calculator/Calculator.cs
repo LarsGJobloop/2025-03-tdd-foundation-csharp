75a96300921c6dd6b87bb2406d1a2579e8836c58
zabronax <zabronax@gmail.com>
2025-03-17 11:35:17 +0100

    pass large number test

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -9,6 +9,12 @@ public class Calculator : ICalculator
 {
   public float Add(float a, float b)
   {
+    var result = a + b;
+    if (!float.IsFinite(result))
+    {
+      throw new ArgumentException($"Numbers must add up to something less then {float.MaxValue} or {float.MinValue}");
+    }
+
     return a + b;
   }
 }
