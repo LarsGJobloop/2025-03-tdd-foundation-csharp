b67f766e57898d251822a750a9fef2dc9f9239a2
zabronax <zabronax@gmail.com>
2025-03-17 10:47:58 +0100

    write first test

---
+++ Calculator.Tests/Calculator.Tests.cs
@@ -0,0 +1,19 @@
+﻿namespace Calculator.Tests;
+
+public class Addition
+{
+    [Fact]
+    public void TwoPlussTwoIsFour()
+    {
+        // Arrange
+        var calculator = new Calculator();
+        float a = 2;
+        float b = 2;
+
+        // Act
+        var result = calculator.Add(a, b);
+
+        // Assert
+        Assert.Equal(4, result);
+    }
+}
